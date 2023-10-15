d7893c24e86f8b232cb46f431656feb71baf2c8f
bitbeckers <code@bitbeckers.com>
2023-10-15 13:33:37 +0200

    chore: Add more verbose logging and message

--- test/foundry/NonceInvalidation.t.sol
+++ test/foundry/NonceInvalidation.t.sol
@@ -77,6 +77,13 @@ contract NonceInvalidationTest is INonceManager, ProtocolBase {
         uint256 newAskNonce = 0 + quasiRandomNumber;
 
         vm.prank(makerUser);
+
+        // ├─ emit NewBidAskNonces(user: 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf, bidNonce: 0, askNonce: 54570651553685478358117286254199992264 [5.457e37])
+        // ├─ [6775] LooksRareProtocol::incrementBidAskNonces(false, true)
+        // │   ├─ emit NewBidAskNonces(user: 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf, bidNonce: 0, askNonce: 0)
+        // │   └─ ← ()
+        // └─ ← "Log != expected log"
+
         vm.expectEmit({checkTopic1: false, checkTopic2: false, checkTopic3: false, checkData: true});
         emit NewBidAskNonces(makerUser, 0, newAskNonce);
         looksRareProtocol.incrementBidAskNonces(false, true);
@@ -121,6 +128,12 @@ contract NonceInvalidationTest is INonceManager, ProtocolBase {
         uint256 newBidNonce = 0 + quasiRandomNumber;
 
         vm.prank(makerUser);
+
+        // ├─ emit NewBidAskNonces(user: 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf, bidNonce: 54570651553685478358117286254199992264 [5.457e37], askNonce: 0)
+        // ├─ [6769] LooksRareProtocol::incrementBidAskNonces(true, false)
+        // │   ├─ emit NewBidAskNonces(user: 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf, bidNonce: 0, askNonce: 0)
+        // │   └─ ← ()
+        // └─ ← "Log != expected log"
         vm.expectEmit({checkTopic1: false, checkTopic2: false, checkTopic3: false, checkData: true});
         emit NewBidAskNonces(makerUser, newBidNonce, 0);
         looksRareProtocol.incrementBidAskNonces(true, false);
